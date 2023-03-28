fc8bd34976d7a214e5333ad4e23085ebbc134f23
jungsungwook <jswcyber@naver.com>
2023-03-28 17:48:57 +0900

    feat:rest api

--- demo/src/api/server.py
+++ demo/src/api/server.py
@@ -3,6 +3,8 @@ import re
 
 import numpy as np
 from abc import ABCMeta
+import requests
+import json
 
 class ServerApi(metaclass=ABCMeta):
     def request(self, server, target):
@@ -10,10 +12,49 @@ class ServerApi(metaclass=ABCMeta):
         서버 정보를 제공 하는 API 입니다.
         """
         try:
-            print("server_response===>", server, target)
-            return f"{server} 서버의 {target} 정보입니다.\n"
+            return_text = f""
+            response = requests.get(f"http://127.0.0.1:8521/server?name={server}&target={target}")
+            if response.status_code != 200:
+                return "서버와의 통신이 원활하지 않습니다."
+            
+            result = json.loads(response.text)
+            if result['status'] == 'error':
+                return "서버와의 통신이 원활하지 않습니다."
+            
+            if(len(result['data']) < 1):
+                return_text += f"{server} 서버에 대한 정보가 없습니다.\n"
+                response = requests.get(f"http://127.0.0.1:8521/server/all")
+                if response.status_code != 200:
+                    return "서버와의 통신이 원활하지 않습니다."
+            
+                result = json.loads(response.text)
+                if result['status'] == 'error':
+                    return "서버와의 통신이 원활하지 않습니다."
+                
+                if(len(result['data']) < 1):
+                    return_text += f"모든 서버에 대한 정보가 없습니다.\n"
+                    return return_text
+                return_text += "대신, 모든 서버에 대한 정보를 알려드리겠습니다.\n"
+                for i in range(len(result['data'])):
+                    return_text += f"{i+1}. {result['data'][i]}\n"
+                return return_text
+            
+            if(len(result['data']) > 1):
+                return_text += "{0} 서버에 대한 정보가 {1}개 있습니다.\n".format(server, len(result['data']))
+                for i in range(len(result['data'])):
+                    return_text += "\n{0}번째 서버의 정보입니다.\n".format(i+1)
+                    return_text += f"서버 이름 : {result['data'][i]['name']}\n"
+                    return_text += f"서버 주소 : {result['data'][i]['ip']}\n"
+                    return_text += f"서버 정보 : {result['data'][i]['info']}\n"
+                    return_text +="====================\n"
+            else:
+                return_text += f"{server} 서버의 정보입니다.\n"
+                return_text += f"서버 이름 : {result['data'][0]['name']}\n"
+                return_text += f"서버 주소 : {result['data'][0]['ip']}\n"
+                return_text += f"서버 정보 : {result['data'][0]['info']}\n"
+            return return_text
         except Exception:
-            return "sorry"
+            return "[server api]문의 바랍니다."
     
     def _flatten_list(self, list_: list) -> list:
         """
